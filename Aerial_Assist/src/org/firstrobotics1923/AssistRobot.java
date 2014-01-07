b5c847ac876494d9fab20ae7751cbfda1a523346
Pavan Hegde <pavan.hegde@rocketmail.com>
2014-01-07 18:58:08 -0500

    Set-up of Project
    Added main class and various folder/packages to guide heirarchy

---
+++ Aerial_Assist/src/org/firstrobotics1923/AssistRobot.java
@@ -0,0 +1,32 @@
+package org.firstrobotics1923;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * The Core Code for FRC Team 1923's "Aerial Assist" Robot
+ * @author Pavan Hegde
+ * @version 0.1
+ * @since Jan. 7, 2014
+ */
+public class AssistRobot extends IterativeRobot{
+  
+    public void roboInit() {
+        
+    }
+    
+    public void autonomousInit() {
+        
+    }
+    
+    public void autonomousPeriodic(){
+        
+    }
+    
+    public void teleopInit() {
+        
+    }
+    
+    public void teleopPeriodic() {
+        
+    }
+}
