f6b4afc1d764805ae5c643ac49ecca4eda9a08e7
chriscush765 <chriscush765@gmail.com>
2014-01-14 19:22:07 -0500

    Piston Deactivator

--- Aerial_Assist/src/org/firstrobotics1923/event/PistonDeactivatorEvent.java
+++ Aerial_Assist/src/org/firstrobotics1923/event/PistonDeactivatorEvent.java
@@ -4,20 +4,19 @@ import org.firstrobotics1923.event.Event;
 import org.firstrobotics1923.Components;
 
 /**
- *
- * @author Christopher
- */
+ * An event that retracts a piston
+ * 
+ * @author Chris Cushman, Aryak Pande, Kiran Chandra
+ * @version 1.0
+ * @since Jan. 14, 2014 
+ **/
 public class PistonDeactivatorEvent extends Event {
 
     public PistonDeactivatorEvent() {
-
-        super(true);
-
+        super(true); //Runs one time only
     }
 
     public void event() {
-
-        Components.samplePneumaticSystem.deactivate();
-
+        Components.samplePneumaticSystem.deactivate(); //retracts piston
     }
 }
